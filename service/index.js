7c2916c76af3a3b0d629d7b18d9509af47929c8a
Easton Crowther <easton.crowther@gmail.com>
2025-03-13 17:30:24 -0600

    began implementing the login backend

--- service/index.js
+++ service/index.js
@@ -1,20 +1,142 @@
-const cookieParser = require('cookie-parser');
-const bcrypt = require('bcryptjs');
-const uuid = require('uuid');
-const express = require('express');
-const app = express();
+    const cookieParser = require('cookie-parser');
+    const bcrypt = require('bcryptjs');
+    const uuid = require('uuid');
+    const express = require('express');
+    const app = express();
 
-// The service port
-const port = process.argv.length > 2 ? process.argv[2] : 3000;
+    // Save scores and users in memory
+    let users = [];
+    let scores = [];
 
-// JSON body parsing
-app.use(express.json());
+    // The service port
+    const port = process.argv.length > 2 ? process.argv[2] : 3000;
 
+    // JSON body parsing
+    app.use(express.json());
 
-app.get('*', (req, res) => {
-    res.send({msg: 'startup service'});
-});
+    // Router for service endpoints
+    var apiRouter = express.Router();
+    app.use('/api', apiRouter);
 
-app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
-});
+    // CreateAuth a new user
+    apiRouter.post('/auth/create', async (req, res) => {
+    if (await findUser('email', req.body.email)) {
+        res.status(409).send({ msg: 'Existing user' });
+    } else {
+        const user = await createUser(req.body.email, req.body.password);
+
+        setAuthCookie(res, user.token);
+        res.send({ email: user.email });
+    }
+    });
+
+    // GetAuth login an existing user
+    apiRouter.post('/auth/login', async (req, res) => {
+    const user = await findUser('username', req.body.username);
+    if (user) {
+        // check passwords
+        if (await bcrypt.compare(req.body.password, user.password)) {
+            // generate an auth token
+            user.token = uuid.v4();
+            setAuthCookie(res, user.token);
+            res.send({ username: user.username });
+            return;
+        }
+    }
+    res.status(401).send({ msg: 'Unauthorized' });
+    });
+
+    // DeleteAuth logout a user
+    apiRouter.delete('/auth/logout', async (req, res) => {
+    const user = await findUser('token', req.cookies[authCookieName]);
+    if (user) {
+        delete user.token;
+    }
+    res.clearCookie(authCookieName);
+    res.status(204).end();
+    });
+
+    // Middleware to verify that the user is authorized to call an endpoint
+    const verifyAuth = async (req, res, next) => {
+        const user = await findUser('token', req.cookies[authCookieName]);
+        if (user) {
+            next();
+        } else {
+            res.status(401).send({ msg: 'Unauthorized' });
+        }
+    };
+
+    // GetScores
+    apiRouter.get('/scores', verifyAuth, (_req, res) => {
+        res.send(scores);
+    });
+
+    // SubmitScore
+    apiRouter.post('/score', verifyAuth, (req, res) => {
+        scores = updateScores(req.body);
+        res.send(scores);
+    });
+
+    // Default error handler
+    app.use(function (err, req, res, next) {
+        res.status(500).send({ type: err.name, message: err.message });
+    });
+
+    // Return the application's default page if the path is unknown
+    app.use((_req, res) => {
+        res.sendFile('index.html', { root: 'public' });
+    });
+
+    // updateScores considers a new score for inclusion in the high scores.
+    function updateScores(newScore) {
+        let found = false;
+        for (const [i, prevScore] of scores.entries()) {
+            if (newScore.score > prevScore.score) {
+                scores.splice(i, 0, newScore);
+                found = true;
+                break;
+            }
+        }
+
+        if (!found) {
+            scores.push(newScore);
+        }
+
+        if (scores.length > 10) {
+            scores.length = 10;
+        }
+
+        return scores;
+    }
+
+    async function createUser(email, password) {
+        const passwordHash = await bcrypt.hash(password, 10);
+
+        const user = {
+            usermane: username,
+            password: passwordHash,
+            token: uuid.v4(),
+        };
+        users.push(user);
+
+        return user;
+    }
+
+    async function findUser(field, value) {
+        if (!value) return null;
+
+        return users.find((u) => u[field] === value);
+    }
+
+    // setAuthCookie in the HTTP response
+    function setAuthCookie(res, authToken) {
+        res.cookie(authCookieName, authToken, {
+            secure: true,
+            httpOnly: true,
+            sameSite: 'strict',
+        });
+    }
+
+    app.listen(port, "0.0.0.0", () => {
+        console.log(`Listening on port ${port}`);
+    });
--- src/login/unauthenticated.jsx
+++ src/login/unauthenticated.jsx
@@ -7,13 +7,26 @@ export function Unauthenticated(props) {
   const [password, setPassword] = React.useState('');
 
   async function loginUser() {
-    localStorage.setItem('userName', userName);
-    props.onLogin(userName);
+    loginOrCreate(`/api/auth/login`);
   }
 
   async function createUser() {
-    localStorage.setItem('userName', userName);
-    props.onLogin(userName);
+    loginOrCreate(`/api/auth/create`);
+  }
+
+  async function loginOrCreate(endpoint) {
+    const res = await fetch(endpoint, {
+      method: 'post',
+      body: JSON.stringify({ username: userName, password: password }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+
+    });
+    if (response?.status === 200) {
+      localStorage.setItem('userName', userName);
+      props.onLogin(userName);
+    } 
   }
 
   return (
